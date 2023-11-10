639af75204658fbc738712f01ab454a92abee3fa
Laila Case <lai_case@hotmail.com>
2023-11-10 10:17:19 +0000

    complete get functionality using express router

--- routers/users.js
+++ routers/users.js
@@ -2,5 +2,15 @@ const express = require("express");
 const usersRouter = express.Router();
 const { check, validationResult } = require("express-validator");
 const { User } = require("../models/index");
+const { use } = require("../src/app");
+
+// CRUD OPERATIONS
 usersRouter.use(express.json());
 usersRouter.use(express.urlencoded({ extended: true }));
+
+usersRouter.get("/", async (req, res) => {
+  const allUsers = await User.findAll();
+  res.json(allUsers);
+});
+
+module.exports = usersRouter;
