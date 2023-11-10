188256486a5a5f61241266c1db48b06d95fe00c2
Laila Case <lai_case@hotmail.com>
2023-11-10 16:02:26 +0000

    add try and catch

--- routers/shows.js
+++ routers/shows.js
@@ -7,76 +7,105 @@ showsRouter.use(express.json());
 showsRouter.use(express.urlencoded({ extended: true }));
 
 // GET ALL
-showsRouter.get("/", async (req, res) => {
-  const allShows = await Show.findAll();
-  res.json(allShows);
-});
+try {
+  showsRouter.get("/", async (req, res, next) => {
+    const allShows = await Show.findAll();
+    res.json(allShows);
+  });
+} catch (error) {
+  next(error);
+}
 
 //GET BY ID
-showsRouter.get("/:id", async (req, res) => {
-  const id = req.params.id;
-  const showById = await Show.findByPk(id);
-  res.json(showById);
-});
+
+try {
+  showsRouter.get("/:id", async (req, res, next) => {
+    const id = req.params.id;
+    const showById = await Show.findByPk(id);
+    res.json(showById);
+  });
+} catch (error) {
+  next(error);
+}
 
 //GET GENRES
-showsRouter.get("/genres/:genre", async (req, res) => {
-  const genre = req.params.genre;
-  const showsByGenre = await Show.findAll({ where: { genre: genre } });
-  res.json(showsByGenre);
-});
+
+try {
+  showsRouter.get("/genres/:genre", async (req, res) => {
+    const genre = req.params.genre;
+    const showsByGenre = await Show.findAll({ where: { genre: genre } });
+    res.json(showsByGenre);
+  });
+} catch (error) {
+  next(error);
+}
 
 //PATCH RATING
-showsRouter.patch(
-  "/:id",
-  [check("rating").not().isEmpty()],
-  async (req, res) => {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      res.json({ error: errors.array() });
-    } else {
-      const id = req.params.id;
-      const reqBody = req.body;
-      const show = await Show.findByPk(id);
-      if (show.rating === null) {
-        show.update({ rating: reqBody.rating })
+
+try {
+  showsRouter.patch(
+    "/:id",
+    [check("rating").not().isEmpty()],
+    async (req, res, next) => {
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+        res.json({ error: errors.array() });
       } else {
-        const newRating = show.rating + reqBody.rating / 2;
-        await show.update({ rating: newRating });
+        const id = req.params.id;
+        const reqBody = req.body;
+        const show = await Show.findByPk(id);
+        if (show.rating === null) {
+          show.update({ rating: reqBody.rating });
+        } else {
+          const newRating = show.rating + reqBody.rating / 2;
+          await show.update({ rating: newRating });
+        }
+        res.json(show);
       }
-      res.json(show);
     }
-  }
-);
+  );
+} catch (error) {
+  next(error);
+}
 
 //PATCH UPDATE STATUS
-showsRouter.patch(
-  "/:id/updates",
-  [
-    check("status").not().isEmpty().trim(),
-    check("status").isLength({ min: 5, max: 25 }),
-  ],
-  async (req, res) => {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      res.json({ error: errors.array() });
-    } else {
-      const id = req.params.id;
-      const reqBody = req.body;
-      const show = await Show.findByPk(id);
-      await show.update({ status: reqBody.status });
-      res.json(show);
+
+try {
+  showsRouter.patch(
+    "/:id/updates",
+    [
+      check("status").not().isEmpty().trim(),
+      check("status").isLength({ min: 5, max: 25 }),
+    ],
+    async (req, res, next) => {
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+        res.json({ error: errors.array() });
+      } else {
+        const id = req.params.id;
+        const reqBody = req.body;
+        const show = await Show.findByPk(id);
+        await show.update({ status: reqBody.status });
+        res.json(show);
+      }
     }
-  }
-);
+  );
+} catch (error) {
+  next(error);
+}
 
 //DELETE SHOW
-showsRouter.delete("/:id", async (req, res) => {
-  const id = req.params.id;
-  const show = await Show.findByPk(id);
-  const name = show.title;
-  await show.destroy();
-  res.send(`${name} deleted`);
-});
+
+try {
+  showsRouter.delete("/:id", async (req, res, next) => {
+    const id = req.params.id;
+    const show = await Show.findByPk(id);
+    const name = show.title;
+    await show.destroy();
+    res.send(`${name} deleted`);
+  });
+} catch (error) {
+  next(error);
+}
 
 module.exports = showsRouter;
--- routers/users.js
+++ routers/users.js
@@ -7,43 +7,64 @@ const { User, Show } = require("../models/index");
 usersRouter.use(express.json());
 usersRouter.use(express.urlencoded({ extended: true }));
 
-usersRouter.get("/", async (req, res) => {
-  const allUsers = await User.findAll();
-  res.json(allUsers);
-});
+//GET USERS
+
+try {
+  usersRouter.get("/", async (req, res) => {
+    const allUsers = await User.findAll();
+    res.json(allUsers);
+  });
+} catch (error) {
+  next(error);
+}
 
 //GET BY ID
-usersRouter.get("/:id", async (req, res) => {
-  const id = req.params.id;
-  const userById = await User.findByPk(id);
-  res.json(userById);
-});
+
+try {
+  usersRouter.get("/:id", async (req, res) => {
+    const id = req.params.id;
+    const userById = await User.findByPk(id);
+    res.json(userById);
+  });
+} catch (error) {
+  next(error);
+}
 
 //GET USER WITH SHOWS
-usersRouter.get("/:id/shows", async (req, res) => {
-  const id = req.params.id;
-  const userWithShows = await User.findOne({
-    where: { id: id },
-    include: Show,
+
+try {
+  usersRouter.get("/:id/shows", async (req, res) => {
+    const id = req.params.id;
+    const userWithShows = await User.findOne({
+      where: { id: id },
+      include: Show,
+    });
+    res.json(userWithShows);
   });
-  res.json(userWithShows);
-});
+} catch (error) {
+  next(error);
+}
 
 //PUT SHOW
-usersRouter.put("/:userid/shows/:showid", async (req, res) => {
-  const userId = req.params.userid;
-  const showId = req.params.showid;
 
-  const userWithShow = await User.findOne({
-    where: { id: userId },
-    include: Show,
+try {
+  usersRouter.put("/:userid/shows/:showid", async (req, res) => {
+    const userId = req.params.userid;
+    const showId = req.params.showid;
+
+    const userWithShow = await User.findOne({
+      where: { id: userId },
+      include: Show,
+    });
+
+    const show = await Show.findByPk(showId);
+
+    await userWithShow.addShow(show);
+
+    res.json(userWithShow);
   });
-
-  const show = await Show.findByPk(showId);
-
-  await userWithShow.addShow(show);
-
-  res.json(userWithShow);
-});
+} catch (error) {
+  next(error);
+}
 
 module.exports = usersRouter;
